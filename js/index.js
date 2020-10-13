0f404c1688abde8be1ca2dc75dd81b974d24d266
sean-one <basement365@gmail.com>
2020-10-13 15:34:51 -0700

    working on safari render issues

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@ lambda_ui.parentNode.addEventListener('click', event => {
 lambda_times.parentNode.addEventListener('click', event => {
     if (!lambda_times.style.transform) {
         lambda_times.style.transform = "rotateY(180deg)";
+        lambda_times.style["-webkit-transform"] = "rotateY(180deg)";
     } else {
         lambda_times.style.transform = "";
     }
