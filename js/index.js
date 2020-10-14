726c100f58d97d049985faa0ba984d4348321f64
sean-one <basement365@gmail.com>
2020-10-14 14:03:31 -0700

    little more clean up

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 const lambda_ui = document.getElementById('lambda-ui');
 const lambda_times = document.getElementById('lambda-times');
-const more_info = document.getElementsByClassName('more-info');
 
 lambda_ui.parentNode.addEventListener('click', event => {
     if (!lambda_ui.style.transform) {
